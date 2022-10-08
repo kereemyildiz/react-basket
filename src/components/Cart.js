d329f71762a12cd6189f0743680a9411385e9dd1
kereemyildiz <a.keremyildiz99@gmail.com>
2022-10-09 01:40:52 +0300

    add number of the basket items near the basket icon

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -8,13 +8,28 @@ const Cart = () => {
 	const increase = context.increase;
 	const decrease = context.decrease;
 	const removeFromBasket = context.removeFromBasket;
+	console.log(basket);
+
+	const totalAmount = basket
+		.reduce(
+			(total, basketItem) =>
+				(total = total + basketItem.price * basketItem.count),
+			0
+		)
+		.toFixed(2);
+
+	const basketItemCount = basket.reduce(
+		(itemCount, basketItem) => (itemCount = itemCount + basketItem.count),
+		0
+	);
 	return (
 		<div>
 			<h2>
-				<Link to="/">Kitap Listesi</Link> <span>Sepetim</span>
+				<Link to="/">Kitap Listesi</Link>{" "}
+				<span>Sepetim {basketItemCount || ""}</span>
 			</h2>
 
-			{basket.length ? <h3>Toplam Sepet Tutarı: 19.99 &#8378;</h3> : ""}
+			{basket.length ? <h3>Toplam Sepet Tutarı: {totalAmount} &#8378;</h3> : ""}
 
 			{basket.map((basketItem) => {
 				return (
